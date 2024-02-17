9fc7fb62fb43541df9a16b702664448d2496e83d
Fhizyel Nazareta <fhizyelblawers@gmail.com>
2024-02-17 20:25:04 +0700

    update portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,7 +41,7 @@ function scrollActive() {
     })
 }
 
-/*===== SCROLL REVEAL ANIMATION =====*/
+// /*===== SCROLL REVEAL ANIMATION =====*/
 const sr = ScrollReveal({
     origin: 'top',
     distance: '80px',
@@ -51,29 +51,29 @@ const sr = ScrollReveal({
 
 /*SCROLL HOME*/
 sr.reveal('.home__title', {})
-sr.reveal('.home__scroll', { delay: 200 })
-sr.reveal('.home__img', { origin: 'right', delay: 400 })
+sr.reveal('.home__scroll', { delay: 100 })
+sr.reveal('.home__img', { origin: 'right', delay: 100 })
 
 /*SCROLL ABOUT*/
-sr.reveal('.about__img', { delay: 500 })
-sr.reveal('.about__subtitle', { delay: 300 })
-sr.reveal('.about__profession', { delay: 400 })
-sr.reveal('.about__text', { delay: 500 })
-sr.reveal('.about__social-icon', { delay: 600, interval: 200 })
+sr.reveal('.about__img', { delay: 100 })
+sr.reveal('.about__subtitle', { delay: 100 })
+sr.reveal('.about__profession', { delay: 200 })
+sr.reveal('.about__text', { delay: 100 })
+sr.reveal('.about__social-icon', { delay: 100, interval: 100 })
 
 /*SCROLL SKILLS*/
 sr.reveal('.skills__subtitle', {})
 sr.reveal('.skills__name', { distance: '20px', delay: 50, interval: 100 })
-sr.reveal('.skills__img', { delay: 400 })
+sr.reveal('.skills__img', { delay: 100 })
 
 /*SCROLL PORTFOLIO*/
-sr.reveal('.portfolio__img', { interval: 200 })
+sr.reveal('.portfolio__img', { interval: 100 })
 
 /*SCROLL CONTACT*/
 sr.reveal('.contact__subtitle', {})
-sr.reveal('.contact__text', { interval: 200 })
-sr.reveal('.contact__input', { delay: 400 })
-sr.reveal('.contact__button', { delay: 600 })
+sr.reveal('.contact__text', { interval: 100 })
+sr.reveal('.contact__input', { delay: 100 })
+sr.reveal('.contact__button', { delay: 100 })
 
 /* ===== Sticky ===== */
 window.addEventListener('scroll', function () {
