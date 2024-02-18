7d6595a93303edcea07b39ceb2ac9906a903bbe5
Fhizyel Nazareta <fhizyelblawers@gmail.com>
2024-02-18 20:43:12 +0700

    optimize website

--- assets/js/portfolio.js
+++ assets/js/portfolio.js
@@ -1,90 +1,90 @@
 let dataPortfolio = [
     {
-        'thumb': 'work1.png',
+        'thumb': 'work1.jpg',
         'name': 'Comovi Green',
         'status':'link',
         'link':'https://drive.google.com/drive/folders/1so2eGTh-KTw3rv-7XmfkHkWVGNgbppmI?usp=sharing'
     },
     {
-        'thumb': 'work15.png',
+        'thumb': 'work15.jpg',
         'name': 'Himti Unas 2020-2022',
         'status':'link',
         'link':'https://drive.google.com/drive/folders/1ZbtKpC0V8gbpDQg1jscQn0qPgzT2lgq2?usp=drive_link'
     },
     {
-        'thumb': 'work2.png',
+        'thumb': 'work2.jpg',
         'name': 'Head Office Cinema XXI',
         'status':'link',
         'link':'https://drive.google.com/drive/folders/10SsrzY3k7UlGWeDLyGnOM2O6jiuMzURS?usp=sharing'
     },
     {
-        'thumb': 'work3.png',
+        'thumb': 'work3.jpg',
         'name': 'Wilx Store',
         'status':'link',
         'link':'https://drive.google.com/drive/folders/1iB8vdBtw2XIaSeA6o1uCruymuwKiV_mM?usp=drive_link'
     },
     {
-        'thumb': 'work4.png',
+        'thumb': 'work4.jpg',
         'name': 'Honda Microsite',
         'status':'link',
         'link':'https://drive.google.com/drive/folders/1StTJfSMXqSfQCZbgGVKLAhf_abNOQ4SV?usp=drive_link'
     },
     {
-        'thumb': 'work5.png',
+        'thumb': 'work5.jpg',
         'name': 'Antv Koplo Superstar',
         'status':'link',
         'link':'https://www.intipseleb.com/koplo-superstar'
     },
     {
-        'thumb': 'work6.png',
+        'thumb': 'work6.jpg',
         'name': 'One Pride MMA',
         'status':'link',
         'link':'https://thevivanetworks.com/prototype/onepride22/'
     },
     {
-        'thumb': 'work7.png',
+        'thumb': 'work7.jpg',
         'name': 'VIAHUB',
         'status':'link',
         'link':'https://thevivanetworks.com/prototype/viahub/'
     },
     {
-        'thumb': 'work8.png',
+        'thumb': 'work8.jpg',
         'name': 'Subdomain Vivacoid',
         'status':'link',
         'link':'https://gadget.viva.co.id/'
     },
     {
-        'thumb': 'work9.png',
+        'thumb': 'work9.jpg',
         'name': 'Bakrie Company Profile',
         'status':'link',
         'link':'https://thevivanetworks.com/prototype/bakrie/'
     },
     {
-        'thumb': 'work10.png',
+        'thumb': 'work10.jpg',
         'name': 'Hut Adira Festival 2023',
         'status':'link',
         'link':'https://www.viva.co.id/adirafestival2023'
     },
     {
-        'thumb': 'work11.png',
+        'thumb': 'work11.jpg',
         'name': 'Sample Microsite Caleg Vivacoid',
         'status':'link',
         'link':'https://thevivanetworks.com/prototype/caleg/demokrat/'
     },
     {
-        'thumb': 'work12.png',
+        'thumb': 'work12.jpg',
         'name': 'Kabar Pemilu TVOne',
         'status':'link',
         'link':'https://www.tvonenews.com/kabar-pemilu'
     },
     {
-        'thumb': 'work13.png',
+        'thumb': 'work13.jpg',
         'name': 'Damai Indonesia',
         'status':'link',
         'link':'https://thevivanetworks.com/prototype/damai-indonesia/'
     },
     {
-        'thumb': 'work14.png',
+        'thumb': 'work14.jpg',
         'name': 'Hut TVOne 16',
         'status':'link',
         'link':'https://www.tvonenews.com/hut-tvone'
